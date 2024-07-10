90edf841f349c73c26eb14ca5a4aa0f6f65ce296
qpwoeir890 <x2702813@gmail.comgit config --global user.email x2702813@gmail.com>
2024-07-10 06:59:03 +0500

    (～﹃～)~zZ

---
+++ js/add.js
@@ -0,0 +1,20 @@
+document.getElementById("languageSelect").addEventListener("change", function() {
+    var selectedLanguage = this.value;
+    
+    // Здесь можно добавить логику для изменения языка на странице
+  });
+
+
+  
+  
+  
+  
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
